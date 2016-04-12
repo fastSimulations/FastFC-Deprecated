4c1a8a6978990403eb7fa8ec123c305cccd8c35e
fastSimulations <support@fastSimulations.com>
2016-04-12 14:39:59 -0700

    updated masterSolver for initMaster

--- appSrc/solvers/meaSolver/meaSolverDemoExtend.C
+++ appSrc/solvers/meaSolver/meaSolverDemoExtend.C
@@ -5,11 +5,11 @@
     \\  /    A nd           | For copyright notice see file Copyright
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
-   F ast           	    | FAST-FC: 
+	F ast           	    | FAST-FC: 
 	is the		            | The Open Source Analysis and Simulation Toolbox 
     A nalysis and           | for Fuel Cells
 	S imulation		        |
-	Toolbox for		        | Copyright 2015, David B. Harvey
+	Toolbox for		        | Copyright 2016, David B. Harvey
 	F uel                   |         
 	C ells                  |                     
 -------------------------------------------------------------------------------
@@ -29,21 +29,8 @@ License
     You should have received a copy of the GNU General Public License
     along with FAST-FC.  If not, see <http://www.gnu.org/licenses/>.
 
-    foam-extend is free software: you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by the
-    Free Software Foundation, either version 3 of the License, or (at your
-    option) any later version.
-
-    foam-extend is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with foam-extend.  If not, see <http://www.gnu.org/licenses/>.
-
 Application
-    meaSolverDemoExtend
+    meaSolverExtend
 
 Description
 	Steady State version of the MEA Steady State Performance Model
@@ -103,7 +90,7 @@ int main(int argc, char *argv[])
 	dimensionedScalar OCV("OCV", dimensionSet( 1, 2, -3, 0, 0, -1, 0 ), 1.1101222924691811);
 
 	// Initialize variables for transport coefficients etc
-	#include <initialization.H>
+	#include <initMaster.H>
 
 	// Create polarization data file
 	{
